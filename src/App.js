30e5312df1b130f97bcabd246dc10331b7c9b603
Rakshit Agarwal <rakshit.agarwal@globalvoxinc.com>
2023-10-02 12:26:57 +0530

    removed unused vars and working code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import {
-  RouterProvider,
-  createBrowserRouter,
+  // RouterProvider,
+  // createBrowserRouter,
   BrowserRouter,
   Routes,
   Route,
--- src/components/store/AuthContext.js
+++ src/components/store/AuthContext.js
@@ -1,5 +1,7 @@
-import React, { useContext, useState } from "react";
-import CartContext from "./CartContext";
+import React, { 
+  // useContext, 
+  useState } from "react";
+// import CartContext from "./CartContext";
 
 const AuthContext = React.createContext({
   token: "",
